185efe9a2cd93e882a4e7678d70331ad4f0a8830
jramirez108 <54561413+jramirez108@users.noreply.github.com>
2019-09-23 19:47:17 -0400

    Update ImportantDate.java

--- ImportantDate.java
+++ ImportantDate.java
@@ -1,4 +1,16 @@
-
 public class ImportantDate {
+	String date, description, category;
 
+
+ImportantDate(String d, String desc, String cat){
+	date = d;
+	description = desc;
+	category = cat;
+}
+
+void edit(String d, String desc, String cat){
+	date = d;
+	description = desc;
+	category = cat;
+}
 }
