cab8ba073c1cc17ac3dd53dac83b15ecbb01b30c
jramirez108 <54561413+jramirez108@users.noreply.github.com>
2019-09-24 20:10:40 -0400

    Add files via upload

--- ImportantDate.java
+++ ImportantDate.java
@@ -1,4 +1,6 @@
-public class ImportantDate {
+import java.io.Serializable;
+
+public class ImportantDate implements Serializable{
 	String date, description, category;
 
 
@@ -13,4 +15,32 @@ void edit(String d, String desc, String cat){
 	description = desc;
 	category = cat;
 }
+
+public String toString(){
+	return date + " " + description + " " + category;
+}
+public String getDate() {
+	return date;
+}
+
+public void setDate(String date) {
+	this.date = date;
+}
+
+public String getDescription() {
+	return description;
+}
+
+public void setDescription(String description) {
+	this.description = description;
+}
+
+public String getCategory() {
+	return category;
+}
+
+public void setCategory(String category) {
+	this.category = category;
+}
+
 }
